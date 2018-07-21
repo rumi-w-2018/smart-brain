893d4939045689b96e71b280dee8190bea665c10
Harumi Warner <harumiwarner@gmail.com>
2018-07-20 18:10:29 -0600

    Updated API url

--- src/actions/config.js
+++ src/actions/config.js
@@ -1,2 +1,3 @@
-export const baseUrl = 'http://localhost:5000/';
+//export const baseUrl = 'http://localhost:5000/';
+export const baseUrl = 'https://sleepy-meadow-67630.herokuapp.com/';
 export const headers = new Headers([['Content-Type', 'application/json']]);
